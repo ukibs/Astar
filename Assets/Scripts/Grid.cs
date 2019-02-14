5de1147e6c871bb2d94ae3d072947845688d1f56
Mzamo <alu.75966@usj.es>
2019-02-14 16:55:58 +0100

    Arreglos, pero no lanzar que peta

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -2,141 +2,151 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Grid : MonoBehaviour {
+public class Grid : MonoBehaviour
+{
 
-	public LayerMask unwalkableMask;
-  public LayerMask costMultiplierMask;
-  public Vector2 gridWorldSize;
-	public float nodeRadius;
-	Node[,] grid;
+    public LayerMask unwalkableMask;
+    public LayerMask costMultiplierMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
 
-	float nodeDiameter;
-	int gridSizeX, gridSizeY;
+    public List<Node> openSet;
+    public HashSet<Node> closedSet;
 
-  public List<Node> openSet;
-	public HashSet<Node> closedSet;
+    public List<Node> path;
 
-  public List<Node> path;
 
-  /***************************************************************************/
+    Node[,] grid;
 
-	void Awake()
-  {
-		nodeDiameter = nodeRadius*2;
-		gridSizeX = Mathf.RoundToInt(gridWorldSize.x/nodeDiameter);
-		gridSizeY = Mathf.RoundToInt(gridWorldSize.y/nodeDiameter);
-		CreateGrid();
-	}
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+    
+    
 
-  /***************************************************************************/
+    /***************************************************************************/
 
-	void CreateGrid()
-  {
-		grid = new Node[gridSizeX,gridSizeY];
-		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x/2 - Vector3.forward * gridWorldSize.y/2;
+    void Awake()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
 
-		for (int x = 0; x < gridSizeX; x ++) {
-			for (int y = 0; y < gridSizeY; y ++) {
-				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
-				bool walkable = !(Physics.CheckSphere(worldPoint,nodeRadius,unwalkableMask));
+    /***************************************************************************/
+
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
                 float costMultiplier =
                     (Physics.CheckSphere(worldPoint, nodeRadius, costMultiplierMask)) ?
                     1.5f : 1.0f;
 
-                grid[x,y] = new Node(walkable, worldPoint, x, y, costMultiplier );
-			}
-		}
-	}
+                grid[x, y] = new Node(walkable, worldPoint, x, y, costMultiplier);
+            }
+        }
+    }
 
-  /***************************************************************************/
+    /***************************************************************************/
 
-	public List<Node> GetNeighbours(Node node, bool eightConnectivity )
+    public List<Node> GetNeighbours(Node node, bool eightConnectivity)
     {
-		List<Node> neighbours = new List<Node>();
+        List<Node> neighbours = new List<Node>();
 
         for (int x = -1; x <= 1; x++)
         {
-			for (int y = -1; y <= 1; y++)
+            for (int y = -1; y <= 1; y++)
             {
-				if( ( x == 0 && y == 0 ) )
+                if ((x == 0 && y == 0))
                 {
-					continue;
+                    continue;
                 }
-                if( !eightConnectivity && ( Mathf.Abs( x ) + Mathf.Abs( y ) > 1 ) ){
+                if (!eightConnectivity && (Mathf.Abs(x) + Mathf.Abs(y) > 1))
+                {
                     continue;
                 }
 
-				int checkX = node.mGridX + x;
-				int checkY = node.mGridY + y;
+                int checkX = node.mGridX + x;
+                int checkY = node.mGridY + y;
 
-				if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY) {
-					neighbours.Add(grid[checkX,checkY]);
-				}
-			}
-		}
+                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+                {
+                    neighbours.Add(grid[checkX, checkY]);
+                }
+            }
+        }
 
-		return neighbours;
-	}
-	
-  /***************************************************************************/
+        return neighbours;
+    }
 
-	public Node NodeFromWorldPoint(Vector3 worldPosition)
+    /***************************************************************************/
+
+    public Node NodeFromWorldPoint(Vector3 worldPosition)
     {
-		float percentX = (worldPosition.x + gridWorldSize.x/2) / gridWorldSize.x;
-		float percentY = (worldPosition.z + gridWorldSize.y/2) / gridWorldSize.y;
-		percentX = Mathf.Clamp01(percentX);
-		percentY = Mathf.Clamp01(percentY);
+        float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+        float percentY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
 
-		int x = Mathf.RoundToInt((gridSizeX-1) * percentX);
-		int y = Mathf.RoundToInt((gridSizeY-1) * percentY);
-		return grid[x,y];
-	}
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+        return grid[x, y];
+    }
 
-  /***************************************************************************/
+    /***************************************************************************/
 
-	void OnDrawGizmos()
+    void OnDrawGizmos()
     {
-		Gizmos.DrawWireCube(transform.position,new Vector3(gridWorldSize.x,1,gridWorldSize.y));
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
 
-		if (grid != null)
+        if (grid != null)
         {
-			foreach (Node n in grid)
+            foreach (Node n in grid)
             {
-				Gizmos.color = (n.mWalkable) ? Color.white:Color.red;
+                Gizmos.color = (n.mWalkable) ? Color.white : Color.red;
 
                 if (openSet != null)
                 {
-					if (openSet.Contains(n)){
-						Gizmos.color = Color.green;
+                    if (openSet.Contains(n))
+                    {
+                        Gizmos.color = Color.green;
                     }
                 }
 
                 if (closedSet != null)
                 {
-					if (closedSet.Contains(n))
+                    if (closedSet.Contains(n))
                     {
-					    Gizmos.color = Color.yellow;
+                        Gizmos.color = Color.yellow;
                     }
                 }
 
                 if (path != null)
                 {
-					if (path.Contains(n))
+                    if (path.Contains(n))
                     {
-						Gizmos.color = Color.black;
+                        Gizmos.color = Color.black;
                     }
                 }
 
-                if ( n.mCostMultiplier > 1.0f)
+                if (n.mCostMultiplier > 1.0f)
                 {
-                  Gizmos.color += Color.blue;
+                    Gizmos.color += Color.blue;
                 }
 
-                Gizmos.DrawCube(n.mWorldPosition, Vector3.one * (nodeDiameter-.1f));
-			}
-		}
-	}
+                Gizmos.DrawCube(n.mWorldPosition, Vector3.one * (nodeDiameter - .1f));
+            }
+        }
+    }
 
-  /***************************************************************************/
+    /***************************************************************************/
 
 }
--- Assets/Scripts/Pathfinding.cs
+++ Assets/Scripts/Pathfinding.cs
@@ -5,95 +5,102 @@ using System.Collections.Generic;
 public class Pathfinding : MonoBehaviour
 {
 
-	public Transform  mSeeker;
-  public Transform  mTarget;   
+    public Transform mSeeker;
+    public Transform mTarget;
 
-  Node              CurrentStartNode;
-  Node              CurrentTargetNode;
+    Node CurrentStartNode;
+    Node CurrentTargetNode;
 
-	Grid              Grid;
+    Grid Grid;
 
-  int               Iterations = 0;
-  float             LastStepTime = 0.0f;
-  float             TimeBetweenSteps = 0.01f;
+    int Iterations = 0;
+    float LastStepTime = 0.0f;
+    float TimeBetweenSteps = 0.01f;
 
-  bool              EightConnectivity = true;
+    bool EightConnectivity = true;
 
 
-  /***************************************************************************/
+    /***************************************************************************/
 
-	void Awake()
+    void Awake()
     {
-		Grid = GetComponent<Grid> ();
+        Grid = GetComponent<Grid>();
 
         Iterations = 0;
         LastStepTime = 0.0f;
-	}
-
-  /***************************************************************************/
-
-	void Update()
-    {
-        // Positions changed?
-        if( PathInvalid() ){
-              // Remove old path
-              if( Grid.path != null ){
-                    Grid.path.Clear();
-              }
-              // Start calculating path again
-              Iterations = 0;
-              if( TimeBetweenSteps == 0.0f ){
-                    Iterations = -1;
-              }
-              FindPath(mSeeker.position, mTarget.position, Iterations );
-        }
-        else{
-              // Path found?
-              if( Iterations >= 0 ){
-                    // One or more iterations?
-                    if( TimeBetweenSteps == 0.0f ){
-                          // One iteration, look until path is found
-                          Iterations = -1;
-                          FindPath(mSeeker.position, mTarget.position, Iterations );
-                    }
-                    else if( Time.time > LastStepTime + TimeBetweenSteps ){
-                          // Iterate increasing depth every time step
-                          LastStepTime = Time.time;
-                          Iterations++;
-                          FindPath(mSeeker.position, mTarget.position, Iterations );
-                    }
-              }
-        }
-	}
-
-  /***************************************************************************/
-
-	bool PathInvalid()
-    {
-        return CurrentStartNode != Grid.NodeFromWorldPoint(mSeeker.position) || 
-            CurrentTargetNode != Grid.NodeFromWorldPoint(mTarget.position) ;
     }
 
-  /***************************************************************************/
-    
-    // List<Node> to worck with the smoothing stuff
-    public void FindPath( Vector3 startPos, Vector3 targetPos, int iterations )
-    {
-		CurrentStartNode  = Grid.NodeFromWorldPoint(startPos);
-		CurrentTargetNode = Grid.NodeFromWorldPoint(targetPos);
+    /***************************************************************************/
 
-		List<Node> openSet      = new List<Node>();
-		HashSet<Node> closedSet = new HashSet<Node>();
-		openSet.Add(CurrentStartNode);
-        Grid.openSet    = openSet;
+    void Update()
+    {
+        // Positions changed?
+        if (PathInvalid())
+        {
+            // Remove old path
+            if (Grid.path != null)
+            {
+                Grid.path.Clear();
+            }
+            // Start calculating path again
+            Iterations = 0;
+            if (TimeBetweenSteps == 0.0f)
+            {
+                Iterations = -1;
+            }
+            FindPath(mSeeker.position, mTarget.position, Iterations);
+        }
+        else
+        {
+            // Path found?
+            if (Iterations >= 0)
+            {
+                // One or more iterations?
+                if (TimeBetweenSteps == 0.0f)
+                {
+                    // One iteration, look until path is found
+                    Iterations = -1;
+                    FindPath(mSeeker.position, mTarget.position, Iterations);
+                }
+                else if (Time.time > LastStepTime + TimeBetweenSteps)
+                {
+                    // Iterate increasing depth every time step
+                    LastStepTime = Time.time;
+                    Iterations++;
+                    FindPath(mSeeker.position, mTarget.position, Iterations);
+                }
+            }
+        }
+    }
+
+    /***************************************************************************/
+
+    bool PathInvalid()
+    {
+        return CurrentStartNode != Grid.NodeFromWorldPoint(mSeeker.position) ||
+            CurrentTargetNode != Grid.NodeFromWorldPoint(mTarget.position);
+    }
+
+    /***************************************************************************/
+
+    // List<Node> to worck with the smoothing stuff
+    public void FindPath(Vector3 startPos, Vector3 targetPos, int iterations)
+    {
+        CurrentStartNode = Grid.NodeFromWorldPoint(startPos);
+        CurrentTargetNode = Grid.NodeFromWorldPoint(targetPos);
+
+        List<Node> openSet = new List<Node>();
+        HashSet<Node> closedSet = new HashSet<Node>();
+        openSet.Add(CurrentStartNode);
+        Grid.openSet = openSet;
 
         int currentIteration = 0;
         Node node = CurrentStartNode;
-		while( openSet.Count > 0 && node != CurrentTargetNode && 
-                ( iterations == -1 || currentIteration < iterations ) )
+        while (openSet.Count > 0 && node != CurrentTargetNode &&
+              (iterations == -1 || currentIteration < iterations))
         {
             // Select best node from open list
-			    node = openSet[0];
+            node = openSet[0];
 
             /****/
             //TODO
@@ -109,47 +116,51 @@ public class Pathfinding : MonoBehaviour
             openSet.Remove(node);
             // Insertar ordenado en vez de con ADD???
             closedSet.Add(node);
-            Grid.openSet    = openSet;
-            Grid.closedSet  = closedSet;
-
+            Grid.openSet = openSet;
+            Grid.closedSet = closedSet;
 
 
             // Check destination
-		    if (node != CurrentTargetNode) {
-
+            if (node != CurrentTargetNode)
+            {
                 // Open neighbours
-                foreach (Node neighbour in Grid.GetNeighbours(node, EightConnectivity)) {
+                foreach (Node neighbour in Grid.GetNeighbours(node, EightConnectivity))
+                {
                     /****/
                     //TODO
                     /****/
-                    // Añadimos los vecinos al openset
                     // First we check that the neighbor is walkable
-                    if (neighbour.mWalkable)
+                    if (!neighbour.mWalkable || closedSet.Contains(neighbour))
                     {
-                        // If the neighbor isn't already in the openset
-                        // or the path to it is shorter than the current
-                        if (!closedSet.Contains(neighbour))
+                        continue;
+                    }
+
+                    // If the neighbor isn't already in the openset
+                    // or the path to it is shorter than the current
+                    if (!openSet.Contains(neighbour) || node.gCost + neighbour.mCostMultiplier < neighbour.gCost)
+                    {
+                        neighbour.hCost = Heuristic(neighbour, CurrentTargetNode);
+                        neighbour.gCost = node.gCost + neighbour.mCostMultiplier;
+                        neighbour.mParent = node;
+                        // Insertar ordenado en vez de con ADD
+                        if (!openSet.Contains(neighbour))
                         {
-                            neighbour.hCost = Heuristic(neighbour, CurrentTargetNode);
-                            neighbour.gCost = node.gCost + 1;
-                            neighbour.mParent = node;
-                            // Insertar ordenado en vez de con ADD
-                            for(int i = 0; i < openSet.Count; i++)
+                            if (openSet.Count == 0)
                             {
-                                if(openSet[i].hCost > node.hCost)
+                                openSet.Add(neighbour);
+                            }
+                            else
+                            {
+                                for (int i = 0; i < openSet.Count; i++)
                                 {
-                                    openSet.Insert(i, neighbour);
+                                    if (openSet[i].hCost >= node.hCost)
+                                    {
+                                        openSet.Insert(i, neighbour);
+                                    }
                                 }
                             }
-                            
-                            //openSet.Add(neighbour);
                         }
-                        //else if (openSet.Find(neighbour).)
-                        //{
-
-                        //}
                     }
-                    
                 }
 
                 currentIteration++;
@@ -157,26 +168,26 @@ public class Pathfinding : MonoBehaviour
             else
             {
                 // Path found!
-                RetracePath(CurrentStartNode,CurrentTargetNode);
+                RetracePath(CurrentStartNode, CurrentTargetNode);
 
                 // Path found
                 Iterations = -1;
 
                 Debug.Log("Statistics:");
-                Debug.LogFormat("Total nodes:  {0}", openSet.Count + closedSet.Count );
-                Debug.LogFormat("Open nodes:   {0}", openSet.Count );
-                Debug.LogFormat("Closed nodes: {0}", closedSet.Count );
+                Debug.LogFormat("Total nodes:  {0}", openSet.Count + closedSet.Count);
+                Debug.LogFormat("Open nodes:   {0}", openSet.Count);
+                Debug.LogFormat("Closed nodes: {0}", closedSet.Count);
 
                 //return closedSet;
             }
-		}
-	}
+        }
+    }
 
-  /***************************************************************************/
+    /***************************************************************************/
 
-	void RetracePath(Node startNode, Node endNode)
+    void RetracePath(Node startNode, Node endNode)
     {
-		List<Node> path = new List<Node>();
+        List<Node> path = new List<Node>();
         Node currentNode = endNode;
 
         /****/
@@ -190,16 +201,17 @@ public class Pathfinding : MonoBehaviour
         }
 
         Grid.path = path;
-	}
+    }
 
-  /***************************************************************************/
+    /***************************************************************************/
 
-	float GetDistance(Node nodeA, Node nodeB)
+    float GetDistance(Node nodeA, Node nodeB)
     {
         // Distance function
         //nodeA.mGridX   nodeB.mGridX
         //nodeA.mGridY   nodeB.mGridY
-        if ( EightConnectivity ){
+        if (EightConnectivity)
+        {
             /****/
             //TODO
             /****/
@@ -216,9 +228,9 @@ public class Pathfinding : MonoBehaviour
         }
     }
 
-  /***************************************************************************/
+    /***************************************************************************/
 
-	float Heuristic(Node nodeA, Node nodeB)
+    float Heuristic(Node nodeA, Node nodeB)
     {
         // Heuristic function
         //nodeA.mGridX   nodeB.mGridX
@@ -229,11 +241,11 @@ public class Pathfinding : MonoBehaviour
         /****/
         GetDistance(nodeA, nodeB);
         return 0;
-	}
+    }
 
     /***************************************************************************/
 
-        // BRESENHAM STUFF --------------------------------------------------------------------------------------------
+    // BRESENHAM STUFF --------------------------------------------------------------------------------------------
 
     /***************************************************************************/
 
@@ -249,7 +261,7 @@ public class Pathfinding : MonoBehaviour
         //TODO: 4 Connectivity
         //TODO: Cost
 
-      int w = x2 - x;
+        int w = x2 - x;
         int h = y2 - y;
         int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
         if (w < 0) dx1 = -1; else if (w > 0) dx1 = 1;
