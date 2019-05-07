5bd343266877dd5f43063190e3280ec0575081c7
Mzamo <alu.75966@usj.es>
2019-05-07 23:41:31 +0200

    Recetas añadidas en interfaz

---
+++ Assets/GUI_recipe.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUI_recipe : MonoBehaviour
+{
+    private Recipe recipe;
+    public int index;
+
+    public Recipe SetIngredient
+    {
+        set
+        {
+            recipe = value;
+            //GetComponent<Image>().sprite = recipe.sprite;
+            GetComponent<Button>().GetComponentInChildren<Text>().text = recipe.name;
+        }
+    }
+
+    public void click()
+    {
+        HUD hud = FindObjectOfType<HUD>();
+        hud.newRecipe(index);
+    }
+}
--- Assets/HUD.cs
+++ Assets/HUD.cs
@@ -7,29 +7,48 @@ public class HUD : MonoBehaviour
 {
     public Button [] ingredientsButtons;
     public Button [] recipesButtons;
+    public Text currentRecipe;
 
-    public IngredientScript [] ingredients;
-    private int index = 0;
+    private IngredientScript [] ingredients;
+    private Recipe[] recipes;
+    private int indexIngredient = 0;
+    private int indexRecipes = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         ingredients = Resources.LoadAll<IngredientScript>("Scriptable/Ingredients");
-        index = 0;
+        recipes = Resources.LoadAll<Recipe>("Scriptable/Recipes");
+        indexIngredient = 0;
         asignIngredients();
+        asignRecipes();
     }
     
+    private void asignRecipes()
+    {
+        for (int i = 0; i < recipesButtons.Length; i++)
+        {
+            recipesButtons[i].GetComponent<GUI_recipe>().index = indexIngredient + i;
+            recipesButtons[i].GetComponent<GUI_recipe>().SetIngredient = recipes[indexRecipes + i];
+        }
+    }
+
     private void asignIngredients()
     {
         for (int i = 0; i < ingredientsButtons.Length; i++)
         {
-            ingredientsButtons[i].GetComponent<GUI_Ingredient>().index = index + i;
-            ingredientsButtons[i].GetComponent<GUI_Ingredient>().SetIngredient = ingredients[index+i];
+            ingredientsButtons[i].GetComponent<GUI_Ingredient>().index = indexIngredient + i;
+            ingredientsButtons[i].GetComponent<GUI_Ingredient>().SetIngredient = ingredients[indexIngredient+i];
         }
     }
 
     public void createPrefab(int index)
     {
         Debug.Log("He pinchado en " + index);
+    }
+
+    public void newRecipe(int index)
+    {
+        currentRecipe.text = recipes[index].name;
     }
 }
